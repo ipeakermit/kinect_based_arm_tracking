6d8c84cc235ec58565b8e22c2109c4f48decd4d4
imchockers <s3682356@student.rmit.edu.au>
2019-06-05 16:54:18 +1000

    added comments

--- scripts/tf_user_tracker.py
+++ scripts/tf_user_tracker.py
@@ -27,6 +27,9 @@ import time
     topic a user's id
 '''
 class UserTracker:
+    '''
+        Default Constructor
+    '''
     def __init__(self):
         rospy.init_node('user_tracker')
         self.sub = rospy.Subscriber("/tf", TFMessage, self.callback)
@@ -34,13 +37,29 @@ class UserTracker:
         self.user_str = None
         self.time_at_user_present = None
 
+    '''
+        Callback, runs on receipt msg on topic /tf
+    '''
     def callback(self, msg):
         user = None
+        # Find a user id from cob_body_tracker child frames within /tf
         for item in msg.transforms:
             if (item.child_frame_id.find("/cob_body_tracker/") != -1 and
                 item.child_frame_id.find("/head") != -1):
+                '''
+                    Extract user id from child frame id in format?: 
+                    /cob_body_tracker/{USER_ID}/head
+                    TODO: It's definitely index 2, the format must be 
+                    slightly different and i cant remember it off the 
+                    top of my head
+                    
+                '''
                 user = item.child_frame_id.split("/")[2]
 
+        '''
+            If user being tracked has not been present for 10 seconds,
+            reset to new user
+        '''
         if (self.time_at_user_present != None and
             time.time() - self.time_at_user_present > 10):
             self.user_str = None
@@ -48,11 +67,19 @@ class UserTracker:
             self.time_at_user_present = None
 
         if user != None:
+            '''
+                If a new user is being tracked, publish the id and
+                set internal variables. Also start a timer
+            '''
             if self.user_str == None:
                 print("tracking " + user)
                 self.user_str = user
                 self.time_at_user_present = time.time()
                 self.pub.publish(user)
+            '''
+                If a user is already being tracked and they appear in
+                the /tf data, reset the timer
+            '''
             elif user == self.user_str:
                 self.time_at_user_present = time.time()
 
